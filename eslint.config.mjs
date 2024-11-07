e16fb4451c8587cd919e35d08c58300b508113ce
Nur Saifuddin <nur.saifuddinnn@gmail.com>
2024-11-08 05:36:45 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable quotes */
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  daStyle,
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+];
